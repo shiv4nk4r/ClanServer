29882cf07c293720238d50d02bed18920766ec04
shiv4nk4r <32880628+shiv4nk4r@users.noreply.github.com>
2020-06-09 14:59:33 +0530

    updated Port

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ app.get("/", (req, res) => {
   res.send("Welcome to the home page");
 });
 
-app.listen(3000, () => console.log("Server is running"));
+const port = process.env.port || 3000;
+app.listen(port, () => console.log("Server is running "));
